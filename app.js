e83c2dcda870491f860c6e72a450b63ec06b11ea
Diego Hernandez <diegohxxiv@gmail.com>
2021-07-13 20:29:47 -0700

    Ocean App
    Files Added To Repository

---
+++ app.js
@@ -0,0 +1,27 @@
+const sideMenuBtn = document.getElementById('side-menu-btn');
+    sideMenu = document.getElementById('side-menu');
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active-nav', window.scrollY > 0)
+})
+sideMenuBtn.addEventListener('click', () => {
+    sideMenuBtn.classList.toggle('active-side-menu-btn');
+    sideMenu.classList.toggle('active-side-menu');
+})
+ScrollOut({
+    once:true,
+    targets:'.about-section',
+});
+
+ScrollOut({
+    once:true,
+    targets:'.client-section',
+});
+ScrollOut({
+    once:true,
+    targets:'.blocks',
+});
+ScrollOut({
+    once:true,
+    targets:'.links'
+});
